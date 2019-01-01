d21a7a0729ea64347aa3b8781dceb9de1f6b602d
gaurav <gauravmishra.190699@gmail.com>
2019-01-01 19:35:25 +0530

    commit number 2 for github

--- application/views/game.php
+++ application/views/game.php
@@ -20,29 +20,30 @@ and open the template in the editor.
         <div class="score-board">
             <div id="user-label" class="badge">user</div>
             <div id="computer-label" class="badge">comp</div>
-            <span id="user-score">0</span>:<span id="computer-score">0</span>           
+            <span id="user-score">0</span>:<span id="computer-score">0</span>
         </div>
-        
+
         <div class="result">
             <p>Paper covers rock. You win!</p>
         </div>
-        
+
         <div class="choices">
             <div class="choice" id="r">
                 <img src="<?php echo base_url(); ?>image/rock_1.jpg" alt="rock">
             </div>
-            
+
             <div class="choice" id="s">
                 <img src="<?php echo base_url(); ?>image/scis_2.jpg" alt="scissors">
             </div>
-            
+
             <div class="choice" id="p">
                 <img src="<?php echo base_url(); ?>image/paper_1.jpg" alt="paper">
             </div>
-            
+
         </div>
         <p id="action-message">Make your move.</p>
-            
+        <p>Just a new change for Git Hub</p>
+
         <?php
         //
         ?>
